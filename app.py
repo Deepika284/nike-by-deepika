35e0fc8e2fbad5acbed4421e674630e710edb8ed
Deepika284 <deepikatanwar@gmail.com>
2025-10-16 14:26:31 +0530

    correctcode

--- app.py
+++ app.py
@@ -371,7 +371,7 @@ HTML_TEMPLATE = '''
         
         .athlete-image {
             width: 100%;
-            height: 110px;
+            height: 300px;
             object-fit: cover;
         }
         
