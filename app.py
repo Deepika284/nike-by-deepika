093d1622e0937bb4ea993e664a67020bdf28ceb2
Deepika284 <deepikatanwar@gmail.com>
2025-10-17 03:16:10 +0530

    fri3am

--- app.py
+++ app.py
@@ -369,7 +369,7 @@ HTML_TEMPLATE = '''
         
         .discover-image {
             width: 100%;
-            height: 420px;
+            height: 600px;
             object-fit: cover;
         }
         
